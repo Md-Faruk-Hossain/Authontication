912c097f63a15f6ca08585ffa420842d9332930e
Md Faruk Hossain <tafimaahmed@gmail.com>
2023-01-10 18:00:07 +0600

    Authonti

---
+++ Authorization/Controllers/RoleController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Authorization.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Authorization.Controllers
+{   
+    [Authorize(Roles ="Admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public RoleController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+           _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(string userrole)
+        {
+            string msg = "";
+            if (!string.IsNullOrEmpty(userrole))
+            {
+                if(await _roleManager.RoleExistsAsync(userrole))
+                {
+                    msg = "Role[" + userrole + "] already exists!!!";
+                }
+                else
+                {
+                    IdentityRole r= new IdentityRole(userrole);
+                    await _roleManager.CreateAsync(r);
+                    msg="Role["+userrole+"] has been create successfull!!!!";
+                }
+            }
+            else
+            {
+                msg = "Please enter a valid role name";
+            }
+            ViewBag.msg = msg;
+            return View("Index");
+
+        }
+        public IActionResult AssignRole()
+        {
+            ViewBag.users = _userManager.Users;
+            ViewBag.roles= _roleManager.Roles;
+            ViewBag.msg = TempData["msg"];
+            return View();
+        } 
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(string userdata, string roledata)
+        {
+            string msg = "";
+            if(!string.IsNullOrEmpty(userdata)&& !string.IsNullOrEmpty(roledata))
+            {
+                ApplicationUser u = await _userManager.FindByEmailAsync(userdata);
+                if (u!=null)
+                {
+                    if(await _roleManager.RoleExistsAsync(roledata))
+                    {
+                        await _userManager.AddToRoleAsync(u, roledata);
+                        msg = "Role has been assign to user!!!";
+                    }
+                }
+                else
+                {
+                    msg = "Role User not found!!!!!";
+                }
+            }
+            else
+            {
+                msg = "Please select a valid user or role!!!";
+            }
+            TempData["msg"]= msg;
+            return RedirectToAction("AssignRole");
+        }
+       
+    }
+}
---
+++ Authorization/Models/DbModels.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authorization.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int  Phone { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime date { get; set; }
+        public bool IsActive { get; set; }
+        public IFormFile Picture { get; set; }
+    }
+}
